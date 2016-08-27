0e6e741187b5d76db1e806509e60d082c5434a14
jeth <jethdeguzman@gmail.com>
2016-08-28 00:18:06 +0800

    Added createGoalCtrl.
    Signed-off-by: jeth <jethdeguzman@gmail.com>

--- www/templates/goal/js/controllers.js
+++ www/templates/goal/js/controllers.js
@@ -3,3 +3,39 @@ appControllers.controller('goalCtrl', function ($scope, $state) {
         $state.go(targetPage);
     };
 });
+
+appControllers.controller('createGoalCtrl', function ($scope, $state, $cordovaImagePicker) {
+    $scope.navigateTo = function (targetPage) {
+        $state.go(targetPage);
+    };
+
+    $scope.selectImage = function (limit) {
+        var options = {
+            maximumImagesCount: limit,
+            width: 1600,
+            height: 900,
+            quality: 100
+        };
+
+        $cordovaImagePicker.getPictures(options)
+            .then(function (results) {
+                $scope.imageList = [];
+                for (var i = 0; i < results.length; i++) {
+                    $scope.imageList.push(results[i]);
+                }
+                alert(JSON.stringify($scope.imageList));
+            }, function (error) {
+                alert(JSON.stringify(error))
+                console.log(error);
+            });
+    };
+
+    $scope.visibilities = [
+        { name : "Private", value : "private" },
+        { name : "Public", value : "public" }
+    ];
+
+    $scope.saveGoal = function(){
+        //http
+    }
+});
