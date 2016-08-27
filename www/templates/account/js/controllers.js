7ae5e75a633c27ca5e4a70e956b95a42a8c3e0c6
jeth <jethdeguzman@gmail.com>
2016-08-28 00:32:22 +0800

    Initial coins.ph integration
    Signed-off-by: jeth <jethdeguzman@gmail.com>

--- www/templates/account/js/controllers.js
+++ www/templates/account/js/controllers.js
@@ -13,7 +13,54 @@ appControllers.controller('accountCtrl', function ($scope, $timeout, $state, $st
     };
 
     $scope.connectCoins = function() {
-        //coins integration
-    }
+        var ref = window.open(
+            'https://coins.ph/user/api/authorize' +
+            '?client_id=l3J6rHubPGLMgiJf4tNe925BR9Am8VfePYWbP5UL' +  
+            '&token_type=Bearer' +
+            '&response_type=token' + 
+            '&grant_type=refresh_token',
+            '_blank'
+        );
+
+        ref.addEventListener('loadstart', function(event) {
+            if (typeof String.prototype.startsWith != 'function') {
+                String.prototype.startsWith = function (str){
+                    return this.indexOf(str) === 0;
+                };
+            }
+
+            console.log('listen event oauth callback');
+
+            if((event.url).startsWith("http://lifegoals.cloudapp.net:8000/")) {
+                console.log('oauth callback url');
+
+                var query = event.url.substr(event.url.indexOf('#') + 1);
+
+                var data = {};
+
+                var parts = query.split('&');
+
+                // read names and values
+                for (var i = 0; i < parts.length; i++) {
+                    var name = parts[i].substr(0, parts[i].indexOf('='));
+                    var val = parts[i].substr(parts[i].indexOf('=') + 1);
+                    val = decodeURIComponent(val);
+                    data[name] = val;
+                }
+
+                console.log('saving auth data ' + JSON.stringify(data));
+
+                data = JSON.stringify(data);
+                window.localStorage.auth = data;
+                alert(data);
+                ref.close();
+            }
+        });
+
+        ref.addEventListener('loadstop', function(event) {
+            alert('loadstop');
+        });
+        
+    };
 
 });
