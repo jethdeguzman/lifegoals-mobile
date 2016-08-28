bf823e607464ed951c9db634612bda33dfe84737
jeth <jethdeguzman@gmail.com>
2016-08-28 09:22:37 +0800

    Implemented create goals.
    Signed-off-by: jeth <jethdeguzman@gmail.com>

--- www/templates/goal/js/controllers.js
+++ www/templates/goal/js/controllers.js
@@ -10,7 +10,13 @@ appControllers.controller('goalCtrl', function ($scope, $state, localStorage) {
 
 });
 
-appControllers.controller('createGoalCtrl', function ($scope, $state, $cordovaImagePicker) {
+appControllers.controller('createGoalCtrl', function ($scope, $state, $cordovaImagePicker, $cordovaFileTransfer, localStorage, $mdToast) {
+    userInfo = localStorage.get("Facebook");
+    $scope.goal = {
+        'name' : '',
+        'targetAmount' : '',
+        'targetDate' : '',
+    };
     $scope.navigateTo = function (targetPage) {
         $state.go(targetPage);
     };
@@ -40,7 +46,47 @@ appControllers.controller('createGoalCtrl', function ($scope, $state, $cordovaIm
     ];
 
     $scope.saveGoal = function(){
-        //http
+        var server = 'http://lifegoals.cloudapp.net/api/v1/goals?user_id=' + userInfo.userId + '&name=' + $scope.goal.name + '&visibility=PRIVATE&target_amount=' + $scope.goal.targetAmount + '&target_date=' +  $scope.formatDate($scope.goal.targetDate);
+        var filePath = $scope.imageList[0];
+        var options = {
+            'user_id' : userInfo.userId,
+            'name' : $scope.goal.name,
+            'visibility' : 'PRIVATE',
+            'target_amount' : $scope.goal.targetAmount,
+            'target_date' : $scope.formatDate($scope.goal.targetDate)
+        }
+
+        $cordovaFileTransfer.upload(server, filePath, options)
+              .then(function(result) {
+                $scope.showToast('Successfully created your goal.');
+                $state.go('app.goal');
+              }, function(err) {
+                $scope.showToast('An error occured.');
+              }, function (progress) {
+                // constant progress updates
+              });
+    };
+
+    $scope.formatDate = function(date){
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        newdate = '' + year + '-' + month + '-' + day;
+        return newdate;
+    }
+
+    $scope.showToast = function (title) {
+        $mdToast.show({
+            controller: 'toastController',
+            templateUrl: 'toast.html',
+            hideDelay: 800,
+            position: 'bottom',
+            locals: {
+                displayOption: {
+                    title: title
+                }
+            }
+        });
     }
 });
 
